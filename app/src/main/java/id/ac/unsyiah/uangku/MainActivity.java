336bf35aeca08d9de55d2102691cdb5427a1949f
Bayu Ramadhan <82028950+BayuRamaddhann@users.noreply.github.com>
2022-05-24 18:25:51 +0700

    Update front

--- app/src/main/java/id/ac/unsyiah/uangku/MainActivity.java
+++ app/src/main/java/id/ac/unsyiah/uangku/MainActivity.java
@@ -2,7 +2,11 @@ package id.ac.unsyiah.uangku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton masukHome = (ImageButton) findViewById(R.id.imageButton);
+
+        masukHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bukaHome = new Intent(getApplicationContext(), HomePengeluaran.class);
+                        startActivity(bukaHome);
+            }
+        });
     }
 }
