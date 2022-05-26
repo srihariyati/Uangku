bfea19fadc46c92dbf91c899f52964273de6e2c5
Bayu Ramadhan <82028950+BayuRamaddhann@users.noreply.github.com>
2022-05-26 22:34:27 +0700

    update front 3

--- app/src/main/java/id/ac/unsyiah/uangku/AddPemasukan.java
+++ app/src/main/java/id/ac/unsyiah/uangku/AddPemasukan.java
@@ -6,14 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class AddPemasukan extends AppCompatActivity {
-    Button back, addpeng;
+    Button back, addpeng, yes, no, buka_pil;
+    ImageButton simpan, batal, pu_simpan;
+    FrameLayout pu_batal, pilihan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pemasukan);
+
+        //set invisible semua pop up --> simpan, batal dan pilihan
+        pu_simpan = (ImageButton) findViewById(R.id.popupberhasil);
+        pu_simpan.setVisibility(View.INVISIBLE);
+
+        pu_batal = (FrameLayout) findViewById(R.id.popupbatal);
+        pu_batal.setVisibility(View.INVISIBLE);
+
+        pilihan = (FrameLayout) findViewById(R.id.pilihan_pem);
+        pilihan.setVisibility(View.INVISIBLE);
+
+        buka_pil = (Button) findViewById(R.id.buka_pilihan);
+        buka_pil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pilihan.setVisibility(View.VISIBLE);
+
+            }
+        });
 
         back = (Button) findViewById(R.id.btn_back);
 
@@ -33,5 +56,49 @@ public class AddPemasukan extends AppCompatActivity {
                 startActivity(bukaAddPeng);
             }
         });
+
+        simpan = (ImageButton) findViewById(R.id.btn_simpan);
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pu_simpan.setVisibility(View.VISIBLE);
+
+                pu_simpan.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent bukaHome = new Intent(getApplicationContext(), HomePengeluaran.class);
+                        startActivity(bukaHome);
+                    }
+                });
+            }
+        });
+
+        batal = (ImageButton) findViewById(R.id.btn_batal);
+        batal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pu_batal.setVisibility(View.VISIBLE);
+
+                yes = (Button) findViewById(R.id.v);
+                no = (Button) findViewById(R.id.x);
+
+                yes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent bukaHome = new Intent(getApplicationContext(), HomePengeluaran.class);
+                        startActivity(bukaHome);
+                    }
+                });
+
+                no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        pu_batal.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
+
+
     }
 }
--- app/src/main/java/id/ac/unsyiah/uangku/AddPengeluaran.java
+++ app/src/main/java/id/ac/unsyiah/uangku/AddPengeluaran.java
@@ -6,14 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class AddPengeluaran extends AppCompatActivity {
-    Button back, addpem;
+    Button back, addpem, yes, no, buka_pil;
+    ImageButton simpan, batal, pu_simpan;
+    FrameLayout pu_batal, pilihan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pengeluaran);
+
+        pu_simpan = (ImageButton) findViewById(R.id.popupberhasil);
+        pu_simpan.setVisibility(View.INVISIBLE);
+
+        pu_batal = (FrameLayout) findViewById(R.id.popupbatal);
+        pu_batal.setVisibility(View.INVISIBLE);
+
+        pilihan = (FrameLayout) findViewById(R.id.pilihan_peng);
+        pilihan.setVisibility(View.INVISIBLE);
 
         back = (Button) findViewById(R.id.btn_back);
 
@@ -33,5 +46,60 @@ public class AddPengeluaran extends AppCompatActivity {
                 startActivity(bukaAddPem);
             }
         });
+
+
+        simpan = (ImageButton) findViewById(R.id.btn_simpan);
+        //saat klik tombol simpan, menampilkan pop up berhasil simpan
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pu_simpan.setVisibility(View.VISIBLE);
+
+                pu_simpan.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent bukaHome = new Intent(getApplicationContext(), HomePengeluaran.class);
+                        startActivity(bukaHome);
+                    }
+                });
+            }
+        });
+
+        batal = (ImageButton) findViewById(R.id.btn_batal);
+        batal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pu_batal.setVisibility(View.VISIBLE);
+
+                yes = (Button) findViewById(R.id.v);
+                no = (Button) findViewById(R.id.x);
+
+                yes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent bukaHome = new Intent(getApplicationContext(), HomePengeluaran.class);
+                        startActivity(bukaHome);
+                    }
+                });
+
+                no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        pu_batal.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
+
+        buka_pil = (Button) findViewById(R.id.buka_pilihan);
+        buka_pil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pilihan.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
     }
 }
